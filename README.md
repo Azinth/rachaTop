a7e4a4a09dc35b54d9c9ff26e723ed612ee1d642
Azinth <gsv.jesus@gmail.com>
2023-08-26 18:39:01 -0300

    first commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# rachaTop
-Site para o Racha Top da Areninha
-
-site criado para gerenciar a lista do jogo que acontece aos domingos.
-Os usuários podem se cadastrar, colocar seu nome na lista, remover seu nome e fazer pagamentos.
---
+++ my-app/src/LoginPage.tsx
@@ -0,0 +1,33 @@
+// LoginPage.tsx
+
+import React, { useState } from 'react';
+
+const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // Aqui você faria uma solicitação ao seu servidor backend para autenticar o usuário
+    console.log(`Logging in with email: ${email} and password: ${password}`);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
